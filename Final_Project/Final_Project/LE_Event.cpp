b15e7c8227b702dac04413a22c714c05940adc5b
Róbert Snær Harðarson <roberth19@ru.is>
2022-03-28 16:48:26 +0000

    shrek

--- Final_Project/Final_Project/LE_Event.cpp
+++ Final_Project/Final_Project/LE_Event.cpp
@@ -2,29 +2,31 @@
 #include "LE_Event.h"
 
 
-LE_Event::LE_Event(Ogre::Vector3 old_vec, Ogre::Vector3 new_vec, LE_Type type)
+LE_Event::LE_Event(GameObject* game_obj, Ogre::Vector3 old_vec, Ogre::Vector3 new_vec, LE_Type type)
 {
 	old_vec_ = old_vec;
 	new_vec_ = new_vec;
 	type_ = type;
+	game_object_ = game_obj;
 }
 
-LE_Event::LE_Event(Ogre::Quaternion old_rot, Ogre::Quaternion new_rot, LE_Type type)
+LE_Event::LE_Event(GameObject* game_obj, Ogre::Quaternion old_rot, Ogre::Quaternion new_rot, LE_Type type)
 {
 	old_rot_ = old_rot;
 	new_rot_ = new_rot;
 	type_ = type;
+	game_object_ = game_obj;
 }
 
 LE_Event::LE_Event(GameObject* src, LE_Type type)
 {
 	switch (type)
 	{
-	case DELETE:
+	case DUPLICATE:
 		type_ = type;
 		game_object_ = src;
 		break;
-	case DUPLICATE:
+	case DELETE:
 		std::cout << "creating action event" << std::endl;
 		is_selected_ = src->is_selected_;
 		mesh_file_name_ = src->entity_->getMesh()->getName();
